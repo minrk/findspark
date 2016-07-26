be8df1feb27d162c46b22dd7b85b9451dd279eb7
Min RK <benjaminrk@gmail.com>
2016-07-26 12:42:11 +0200

    release 1.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changes in findspark
+
+## 1.1.0
+
+Add `findspark.add_packages`
+
+
+## 1.0.0
+
+First release
--- findspark.py
+++ findspark.py
@@ -8,7 +8,7 @@ from glob import glob
 import os
 import sys
 
-__version__ = '1.0.0.dev'
+__version__ = '1.1.0'
 
 
 def find():
